4cf5cf894b76dd19fbc55083ca1fa4c4968a6311
BarryTheBee571 <yuan.zhuang9@education.nsw.gov.au>
2025-05-02 10:14:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from game_objects import Player, Weapon
 import random
 
-Player1= Player('Bartholomew of Scotland III', 'Asian', '')
+Player1= Player('Bartholomew of Scotland III', 'Marsian', 'Trumpet Blower', 20, 200)
+Player2= Player('Little Timmy', 'Gnome', 'Anklebiter', 50, 20)
